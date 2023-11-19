91c7aae62dff6e329b91052ebd635a9da36271ce
Olawale <henry.williams658@gmail.com>
2023-11-19 01:26:08 +0000

    Test if store can be created with empty fields

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a Store Name: "
+store_name = gets.chomp
+
+new_store = Store.create(
+  name: store_name,
+)
+
+# Save the record and display error messages if validation fails
+if new_store.save
+  puts "Store successfully created!"
+else
+  puts "Failed to create store. Errors:"
+  new_store.errors.full_messages.each do |error_message|
+    puts "- #{error_message}"
+  end
+end
